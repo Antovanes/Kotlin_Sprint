766bbd880ed4c95e4578069d6eb8e07a82723839
Anton <Antovanesyan@gmail.com>
2025-04-08 19:22:33 +0700

    KS-1-3 space flight (1 correction)

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -4,14 +4,14 @@ fun main () {
     // Задача 3 к Уроку 1
 
     val yearFFSpace: Int = 1961  // FFSpace  = first flight into space (первый полёт в космос)
-    var hourFFSpace: Short = 9
-    var minuteFFSpace: Short = 7
+    var hourFFSpace: String = "09"
+    var minuteFFSpace: String = "07"
 
     println("Год первого полёта в космос: $yearFFSpace")
-    print("Время старта: $hourFFSpace часов ")
-    println("$minuteFFSpace минут")
+    print("Время старта: $hourFFSpace:")
+    println(minuteFFSpace)
 
-    hourFFSpace = 10
-    minuteFFSpace = 55
+    hourFFSpace = "10"
+    minuteFFSpace = "55"
     println("Время посадки: $hourFFSpace:$minuteFFSpace")
 }
